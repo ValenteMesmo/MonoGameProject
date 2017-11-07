b7d694446248294f5b95120c846931da95cd29ad
Ricardo Valente <valentemesmo@gmail.com>
2017-11-07 07:30:37 -0200

    Fixing item location

--- SharedContent/Things/Player.cs
+++ SharedContent/Things/Player.cs
@@ -9,6 +9,7 @@ namespace MonoGameProject
     {
         //planning: 
         //z2 enemy with shield
+        //handle damage even when on cooldown...(if last damage < current damage... )
 
         //  char select. choose which controller use (the one who presses start)
         //agora já tem a logica de matar o player esmagando... que tal deixar o boss sólido?
